80a109c8bd12b9d6b6e77495ad92150ba4b69363
Muhammad Zain Alam <muhamadzainalam.dev@gmail.com>
2026-03-12 01:44:34 +0500

    Updating AWS Keys

--- controllers/PostControllers/AWSCommands.js
+++ controllers/PostControllers/AWSCommands.js
@@ -4,24 +4,29 @@ import {
   PutObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const s3Client = new S3Client({
-  region: "ap-south-1",
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: "REMOVED",
-    secretAccessKey: "REMOVED",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
+
+const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
 
 const GetReadingUrl = async (req, res) => {
   const { filename } = req.body;
 
   const command = new GetObjectCommand({
-    Bucket: "synk-app-media-bucket",
+    Bucket: BUCKET_NAME,
     Key: `images/${filename}`,
   });
 
-  const URL = await getSignedUrl(s3Client, command);
+  const URL = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
 
   res.json(URL);
 };
@@ -29,15 +34,13 @@ const GetReadingUrl = async (req, res) => {
 const GetUploadingUrl = async (req, res) => {
   const { filename, contentType } = req.body;
 
-  console.log(filename, contentType);
-
   const command = new PutObjectCommand({
-    Bucket: "synk-app-media-bucket",
+    Bucket: BUCKET_NAME,
     Key: `images/${filename}`,
     ContentType: contentType,
   });
 
-  const URL = await getSignedUrl(s3Client, command);
+  const URL = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
 
   res.json(URL);
 };
--- controllers/UserControllers/GetFromuserNameController.js
+++ controllers/UserControllers/GetFromuserNameController.js
@@ -3,8 +3,6 @@ import User from "../../models/UserModel.js";
 const GetFromuserName = async (req, res) => {
   const { user_Name } = req.body;
 
-  console.log(user_Name);
-
   const user = await User.findOne({ user_Name });
 
   res.json(user);
