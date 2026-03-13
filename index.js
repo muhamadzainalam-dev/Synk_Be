d1b7dc516549b494ebc9404d09d89e448d63d516
Muhammad Zain Alam <muhamadzainalam.dev@gmail.com>
2026-03-14 02:56:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = express();
 dotenv.config();
 server.use(
   cors({
-    origin: ["http://localhost:5173", "https://synk-beta.vercel.app"],
+    origin: ["http://localhost:5173", "https://synk-fe.vercel.app"],
     credentials: true,
   }),
 );
